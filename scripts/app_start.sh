593936084090625970ea821fd9859a07b8fd3e44
Satya Srikant Mantha <Satya Srikant Mantha>
2017-05-07 22:43:24 +0530

    updated jar start command

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /home/ec2-user/app/gs-rest-service-0.1.0.jar >> /home/ec2-user/application.log &
+java -jar /home/ec2-user/app/gs-rest-service-0.1.0.jar 2> /home/ec2-user/appError.log > /home/ec2-user/appOutput.log &
